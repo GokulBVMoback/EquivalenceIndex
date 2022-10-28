a5497be335b846fbbb0ff9f59154cca52c3bcfa1
GokulBVMoback <gokulbv@moback.com>
2022-10-28 18:17:37 +0530

    Equivalence index checking

---
+++ EquivalenceNumber/EquivalenceNumber/EquiInd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquivalenceNumber
+{
+    public class EquiInd
+    {
+        public int Find(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int lsum = 0;
+                int rsum = 0;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    rsum = rsum + (numbers[j]);
+
+                }
+                for (int k = 0; k < i; k++)
+                {
+                    lsum = lsum + (numbers[k]);
+                }
+                if (lsum == rsum)
+                {
+                    return i;
+                }
+            }
+            //TODO - add solution here
+            return -1;
+        }
+    }
+}
---
+++ EquivalenceNumber/EquivalenceNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquivalenceNumber
+{
+    internal class Program
+    {
+        //int[] num;
+       
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 3, 2, 3, 3, 3 };
+            int[] num2={ 1, 2, 3, 4, 5, 7, 8, 10, 12 };
+            int[] num3 = { 2, 2, 3, 4, 3, 74, 8, 10, 12 };
+            int[] num4 = { 10, 11, 3, 1, 1, 1, 9, 9 };
+            int[] num5 = { 4, 3, 5, 6, 7, 5, 10, 15 };
+            EquiInd equiInd = new EquiInd();
+            Console.WriteLine(equiInd.Find(num));
+            Console.WriteLine(equiInd.Find(num2));
+            Console.WriteLine(equiInd.Find(num3));
+            Console.WriteLine(equiInd.Find(num4));
+            Console.WriteLine(equiInd.Find(num5));  
+        }
+
+    }
+}
